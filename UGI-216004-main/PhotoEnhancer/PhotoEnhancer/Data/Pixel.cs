22b1c792c64f318334b43e965f7d20dc1a1be3ba
Полина Мясоедова <polyaflaww@gmail.com>
2023-06-04 18:59:03 +0500

    задание 2, вариант 21

---
+++ UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Data/Pixel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public struct Pixel
+    {
+        private double r;
+        public double R 
+        {
+            get => r;
+            set  => r = CheckValue(value); 
+        }
+
+        private double g;
+        public double G
+        {
+            get => g;
+            set => g = CheckValue(value);
+        }
+
+        private double b;
+        public double B
+        {
+            get => b;
+            set => b = CheckValue(value);
+        }
+
+        public Pixel(double red, double green, double blue) : this()
+        {
+            R = red;
+            G = green;
+            B = blue;
+        }
+
+        public static Pixel operator *(double k, Pixel p)
+        {
+            Pixel result = new Pixel();
+
+            result.r = Trim(k * p.r);
+            result.g = Trim(k * p.g);
+            result.b = Trim(k * p.b);
+
+            return result;
+        }
+
+        public static Pixel operator *(Pixel p, double k) => k * p;      
+
+        private double CheckValue(double val)
+        {
+            if (val < 0 || val > 1)
+                throw new ArgumentException("Неверное значение яркости канала");
+
+            return val;
+        }
+
+        private static double Trim(double lightness)
+        {
+            if(lightness > 1)
+                return 1;
+
+            return lightness;
+        }
+    }
+}
